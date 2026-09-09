df9c713d4a86c1038c3c418fa9c07773ed8072a1
UMMADIPRAVALLIKA2115 <ummadipravallika@gmail.com>
2026-09-09 14:14:27 +0530

    Clean requirements and remove duplicates

--- device_simulator.py
+++ device_simulator.py
@@ -2,49 +2,100 @@ import requests
 import time
 import random
 
+
+# ============================================================
 # LOCAL FLASK SERVER
+# ============================================================
+
 URL = "http://127.0.0.1:5000/ingest"
 
+
+# ============================================================
 # 3 MACHINES
+# ============================================================
+
 machines = [
     "CNC-HYD-001",
     "LATHE-HYD-002",
     "DRILL-HYD-003"
 ]
 
+
+# ============================================================
+# LIVE SIMULATION
+# ============================================================
+
 while True:
 
-    for m_id in machines:
+    for machine_id in machines:
 
-        temperature = random.randint(60, 95)
-        vibration = round(random.uniform(0.1, 1.0), 2)
+        # ----------------------------------------------------
+        # Generate live temperature
+        # ----------------------------------------------------
+
+        temperature = random.randint(
+            60,
+            95
+        )
+
+
+        # ----------------------------------------------------
+        # Generate live vibration
+        # ----------------------------------------------------
+
+        vibration = round(
+            random.uniform(
+                0.10,
+                1.00
+            ),
+            2
+        )
+
+
+        # ----------------------------------------------------
+        # Payload
+        # ----------------------------------------------------
 
         payload = {
-            "machine_id": m_id,
+
+            "machine_id": machine_id,
+
             "temperature": temperature,
+
             "vibration": vibration
+
         }
+
 
         try:
 
             response = requests.post(
+
                 URL,
+
                 json=payload,
+
                 timeout=5
+
             )
+
 
             print(
                 f"📡 Sent to Cloud: "
-                f"{m_id} | "
+                f"{machine_id} | "
                 f"Temp: {temperature}°C | "
                 f"Vibration: {vibration}G | "
-                f"{response.status_code}"
+                f"HTTP: {response.status_code}"
             )
+
 
         except requests.exceptions.RequestException as error:
 
             print(
-                f"❌ Server unavailable: {error}"
+                f"❌ Flask server unavailable: "
+                f"{error}"
             )
 
+
+    # Wait 2 seconds
     time.sleep(2)
